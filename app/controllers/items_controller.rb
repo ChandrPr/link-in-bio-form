6bdcbdf93bf89c886a6c5fc40a8c6de316ab5012
ChandrPr <praphulla91@gmail.com>
2024-11-09 16:09:36 +0000

    looking at delete functionality.  not working yet.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,25 @@
 class ItemsController < ApplicationController
   def index
     @list_of_items = Item.all
-
     render({ :template => "item_templates/list" })
+  end
+
+  def backdoor
+    @list_of_items = Item.order({:created_at => :desc})
+    render({ :template => "item_templates/backdoor"})
+  end
+
+  def insert_item
+    item = Item.new
+    item.link_description = params['query_link_description']
+    item.link_url = params["query_link_url"]
+    item.thumbnail_url = params['query_thumbnail_url']
+    item.save
+    redirect_to("/")
+  end
+
+  def delete_item
+    Item.where({:id => params[:id]})[0].destroy
+    redirect_to("/backdoor")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "backdoor"})
+  post("/insert_item", {:controller => "items", :action => "insert_item"})
+  delete("/delete_item/:id", {:controller => "items", :action => "delete_item"})
 end
